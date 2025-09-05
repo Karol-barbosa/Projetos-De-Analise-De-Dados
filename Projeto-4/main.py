9024debfefdd1e2951584933746ed0ca8ee9f7c6
Karol <karol.barbosa@sou.inteli.edu.br>
2025-09-05 08:37:02 -0300

    adicina o projeto 4

---
+++ Projeto-4/main.py
@@ -0,0 +1,8 @@
+import time_series_visualizer as ts
+
+# Gerar os gráficos
+ts.draw_line_plot()
+ts.draw_bar_plot()
+ts.draw_box_plot()
+
+print("✅ Gráficos salvos: line_plot.png, bar_plot.png, box_plot.png")
